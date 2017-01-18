c9cc8543615b66200ee08b2f2d449c0fe4c62694
dinar dalvi <dinar@applatix.com>
2017-01-18 10:37:40 -0800

    Issue #3: update sleep time

--- main_test.go
+++ main_test.go
@@ -5,8 +5,8 @@ import "fmt"
 
 func TestSomething(t *testing.T) {
 	fmt.Println(logo)
-        fmt.Println("sleep 20 seconds")
-	for i := 0; i < 20; i++ {
+        fmt.Println("sleep 21 seconds")
+	for i := 0; i < 21; i++ {
 		time.Sleep(time.Second)
 		fmt.Printf("slept %v seconds\n", i)
 	}
